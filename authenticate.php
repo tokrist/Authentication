ba5dd96ad3bcc539b70bc3518207b51ef9519c4a
Kristof Toth <68947997+tokrist@users.noreply.github.com>
2021-06-22 13:37:43 +0200

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,8 +1,8 @@
 <?php
     function authenticate($action, $string) {
-        $output = "";
-        $encrypt_method = "AES-256-CBC";
-        $key = "SECRET_KEY_HERE"; // needs to be 16 characters
+        $output = '';
+        $encrypt_method = 'AES-256-CBC';
+        $key = 'SECRET_KEY_HERE'; // needs to be 16 characters
         $iv = substr(hash('sha256', $key), 0, 16);
         
         if($action == 'encrypt') {
@@ -11,7 +11,7 @@
         } else if($action == 'decrypt') {
             $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
         } else {
-            $output = "Undefined authentication method!";
+            $output = 'Undefined authentication method!';
         }
         
         return $output;
@@ -25,7 +25,7 @@
         } else if($action == 'decrypt') {
             $output = password_verify($string, $password); // $sting is the input (plain text), $password is the hash that you queryed or stored
         } else {
-            $output = "Undefined authentication method!";
+            $output = 'Undefined authentication method!';
         }
         
         return $output;
