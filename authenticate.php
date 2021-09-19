6dfc7bd0b8775c962f28b2b81ca7e9a4683c4ba5
Kristof <68947997+tokrist@users.noreply.github.com>
2021-09-19 11:54:15 +0200

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -7,36 +7,28 @@
  */
 <?php
     function authenticate($action, $string) {
-        $output = '';
         $encrypt_method = 'AES-256-CBC';
         $key = 'SECRET_KEY_HERE'; // needs to be 16 characters
         $iv = substr(hash('sha256', $key), 0, 16);
         
-        if (strlen($key) != 16) return $output = "Invalid key length!";
+        if (strlen($key) != 16) return 'Invalid key length!';
 
         if($action == 'encrypt') {
-            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
-            $output = base64_encode($output);
+            return base64_encode(openssl_encrypt($string, $encrypt_method, $key, 0, $iv));
         } else if($action == 'decrypt') {
-            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+            return openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
         } else {
-            $output = 'Undefined authentication method!';
+            return 'Undefined authentication method!';
         }
-        
-        return $output;
     }
 
     function password($action, $string) {
-        $output = "";
-        
         if($action == 'encrypt') {
-            $output = password_hash($string, PASSWORD_ARGON2ID, ['memory_cost' => 2048, 'time_cost' => 4, 'threads' => 3]);
+            return password_hash($string, PASSWORD_ARGON2ID, ['memory_cost' => 2048, 'time_cost' => 4, 'threads' => 3]);
         } else if($action == 'verify') {
-            $output = password_verify($string, $password); // $string is the input (plain text), $password is the hash that you queryed or stored
+            return password_verify($string, $password); // $string is the input (plain text), $password is the hash that you queryed or stored
         } else {
-            $output = 'Undefined authentication method!';
+            return 'Undefined authentication method!';
         }
-        
-        return $output;
     }
 ?>
